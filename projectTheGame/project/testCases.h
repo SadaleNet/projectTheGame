6a632f2f13c4ed21fe8b50991ee597ead3c66b15
wongcc966422 <wongcc966422@users.noreply.github.com>
2014-02-10 09:00:39 +0800

    Update testCases.h

--- projectTheGame/project/testCases.h
+++ projectTheGame/project/testCases.h
@@ -3,6 +3,7 @@
 #include "./db/FileDb.h"
 #include "./db/RemoteDb.h"
 #include <iostream>
+#include <stdexcept>
 
 void testCases(){
 	#ifndef NDEBUG
@@ -15,7 +16,7 @@ void testCases(){
 	}
 	try{
 		RemoteDb::test();
-	}catch(std::runtime_error&){
+	}catch(std::runtime_error& e){
 		std::cerr <<"Unable to test RemoteDb: \n" <<e.what() <<std::endl;
 	}
 	std::cerr <<"Congratz! All test cases were passed successfully. :D" <<std::endl;

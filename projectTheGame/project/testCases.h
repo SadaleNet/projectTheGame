b6d1951cc8043633a6e004d55eea7342eefb66b5
wongcc966422 <wongcc966422@users.noreply.github.com>
2014-02-10 08:59:42 +0800

    testCases.h: added error handling.

--- projectTheGame/project/testCases.h
+++ projectTheGame/project/testCases.h
@@ -8,11 +8,15 @@ void testCases(){
 	#ifndef NDEBUG
 	Vec2::test();
 	Mat33::test();
-	FileDb::test();
+	try{
+		FileDb::test();
+	}catch(std::runtime_error& e){
+		std::cerr <<"Unable to test LocalDb: \n" <<e.what() <<std::endl;
+	}
 	try{
 		RemoteDb::test();
 	}catch(std::runtime_error&){
-		std::cerr <<"Unable to test RemoteDb: connection failed." <<std::endl;
+		std::cerr <<"Unable to test RemoteDb: \n" <<e.what() <<std::endl;
 	}
 	std::cerr <<"Congratz! All test cases were passed successfully. :D" <<std::endl;
 	#endif
